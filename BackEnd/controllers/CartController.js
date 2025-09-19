a8cb2f84f6546b315e922998a5f1c42be5cfe326
“Ravi <sobwaniravi@gmail.com>
2025-09-19 15:05:38 +0530

    Implement Cart API: Update, and Delete Cart functionality

--- BackEnd/controllers/CartController.js
+++ BackEnd/controllers/CartController.js
@@ -15,7 +15,8 @@ const addItems = async (req, res) => {
 
     if (userCard) {
       userCard.cartItems = cartItems;
-      userCard.cartItems = cartItems;
+      userCard.totalAmount = totalAmount;
+      await userCard.save();
     } else {
       const addedItems = new Cart({
         userId,
@@ -32,5 +33,70 @@ const addItems = async (req, res) => {
   } catch (error) {
     console.log("Error Occrured", error);
     res.status(500).json({ status: false, message: "Internal Server Error" });
+  }
+};
+
+const updateCart = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const { cartItems, totalAmount } = req.body;
+
+    if (!userId || !cartItems || !totalAmount) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Enter Valid Details" });
+    }
+
+    const requiredCart = await Cart.findOne({ userId });
+
+    if (!requiredCart) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Required Cart Does not Exist" });
+    }
+    requiredCart.cartItems = cartItems;
+    requiredCart.totalAmount = totalAmount;
+
+    await requiredCart.save();
+
+    return res
+      .status(200)
+      .json({ status: true, message: "Cart Updated SuccessFully" });
+  } catch (error) {
+    console.log("Error Occured While Updating Cart:", error);
+    return res
+      .status(500)
+      .json({ status: false, message: "Internal Server Error" });
+  }
+};
+
+const emptyCart = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Enter The valid UserId" });
+    }
+
+    const userCart = await Cart.findOne({ userId });
+
+    if (!userCart) {
+      return res
+        .status(404)
+        .json({ message: "Required Usercart does not Exist" });
+    }
+
+    const cartTodelete = await Cart.deleteOne({ userId });
+    return res
+      .status(200)
+      .json({ status: true, message: "Cart Removed SuccessFully" });
+  } catch (error) {
+    console.log("Error Occured While deleting Cart:", error);
+    return res
+      .status(500)
+      .json({ status: false, message: "Internal Server Error" });
   }
 };
