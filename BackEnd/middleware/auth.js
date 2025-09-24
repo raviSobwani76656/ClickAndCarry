61a8b19f37a72e20e851235866da3cffeb3a71da
“Ravi <sobwaniravi@gmail.com>
2025-09-24 18:59:38 +0530

    feat(auth): implement JWT authentication middleware with refresh token support

---
+++ BackEnd/middleware/auth.js
@@ -0,0 +1,57 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const crypto = require("crypto");
+const { accessToken } = require("../utils/jwtTokenGenerator");
+
+const authenticateUser = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    let token = authHeader && authHeader.split(" ")[1];
+
+    console.log(token);
+
+    if (!token) {
+      const refreshToken = req.cookies?.refreshToken;
+
+      if (!refreshToken) {
+        return res
+          .status(401)
+          .json({ status: false, message: "Not Authenticated" });
+      }
+
+      const tokenHash = crypto
+        .createHash("sha256")
+        .update(refreshToken)
+        .digest("hex");
+
+      const now = new Date();
+      const user = await User.findOne({
+        "refreshTokens.tokenHash": tokenHash,
+        "refreshTokens.expiresAt": { $gt: now },
+      });
+
+      if (!user)
+        return res
+          .status(403)
+          .json({ status: false, message: "Invalid or expired refresh token" });
+
+      // Generate new access token
+      token = accessToken({ id: user._id, email: user.email });
+      res.setHeader("x-access-token", token);
+
+      req.user = { id: user._id };
+      return next();
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = { id: decoded.id };
+    next();
+  } catch (error) {
+    console.error(error);
+    res
+      .status(403)
+      .json({ status: false, message: "Token invalid or expired" });
+  }
+};
+
+module.exports = authenticateUser;
