5a4bac3a1cbf3fc270623047cc57e2988679b310
“Ravi <sobwaniravi@gmail.com>
2025-09-30 14:33:36 +0530

    Feat:Added the getAll orders API endpoint to fetch all the orders placed by user

--- BackEnd/routes/OrderRoutes.js
+++ BackEnd/routes/OrderRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { cancelOrder, createOrder } = require("../controllers/OrderController");
+const {
+  cancelOrder,
+  createOrder,
+  getAllOrders,
+} = require("../controllers/OrderController");
 const authenticateUser = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/", authenticateUser, createOrder);
 router.delete("/:id", authenticateUser, cancelOrder);
+router.get("/", authenticateUser, getAllOrders);
 
 module.exports = router;
