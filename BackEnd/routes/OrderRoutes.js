f7017feac072cb40e60a7bebb10423405d72fb39
“Ravi <sobwaniravi@gmail.com>
2025-09-30 14:57:35 +0530

    feat:Add the get single order API endpoint to fetch a particular user

--- BackEnd/routes/OrderRoutes.js
+++ BackEnd/routes/OrderRoutes.js
@@ -3,6 +3,7 @@ const {
   cancelOrder,
   createOrder,
   getAllOrders,
+  getSingleOrder,
 } = require("../controllers/OrderController");
 const authenticateUser = require("../middleware/auth");
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/", authenticateUser, createOrder);
 router.delete("/:id", authenticateUser, cancelOrder);
 router.get("/", authenticateUser, getAllOrders);
+router.get("/:id", getSingleOrder);
 
 module.exports = router;
